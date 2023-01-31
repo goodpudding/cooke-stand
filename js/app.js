92e837d3e9cdcaa0697d43d426d52cc682f3d304
Trey Young(goodpudding) <james.trey.young@gmail.com>
2023-01-31 09:38:20 -0800

    added files missing

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 'use strict';
 
 // Setting up Hours
-let hours = ['6 a.m.', '7 a.m.', '8 a.m.', '9 a.m.', '10 a.m.', '11 a.m.','12 p.m.','1 p.m.','2 p.m.','3 p.m.','4 p.m.','5 p.m.','6 p.m.','7 p.m.','8 p.m.', 'Total: ']
+let hours = ['6 a.m.', '7 a.m.', '8 a.m.', '9 a.m.', '10 a.m.', '11 a.m.','12 p.m.','1 p.m.','2 p.m.','3 p.m.','4 p.m.','5 p.m.','6 p.m.','7 p.m.', 'Total: ']
 let storeHourDisplay = document.createElement('section');
 let ul = document.createElement('ul');
 let seattleTotalCookieSales = 0;
@@ -12,27 +12,24 @@ let seattleStore = {
   min: 23,
   max: 65,
   avgSales: 6.3,
-  cookieSales: [],
+  cookieSalesArray: [],
   getNumberOfRandomCustomers: function() {
     return Math.floor(Math.random() * (this.max - this.min + 1) + this.min);
   },
   getCookiesPerHour: function(){
     return Math.round((this.getNumberOfRandomCustomers() * this.avgSales));
   }
-}
-
-
+};
 
 // For loop to create a random number per index of the array hours
 for (let i = 0; i < hours.length; i++){
   let storeHour = document.createElement('li');
-  storeHour.textContent = seattleStore.cookieSales[i] = seattleStore.getCookiesPerHour();
+  storeHour.textContent = seattleStore.cookieSalesArray[i] = seattleStore.getCookiesPerHour();
   ul.appendChild(storeHour);
- seattleTotalCookieSales += seattleStore.cookieSales[i];
- console.log(seattleTotalCookieSales);
- 
+  seattleTotalCookieSales += seattleStore.cookieSalesArray[i];
+  console.log(seattleTotalCookieSales);
+
 }
 let seattleStoreContainer =document.getElementById('seattleStoreID');
 seattleStoreContainer.appendChild(ul);
 
- 
